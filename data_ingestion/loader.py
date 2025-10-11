a7c0b998673e7819d873081269f6811e20b393c4
rzseq48 <rohanseq48@gmail.com>
2025-10-11 12:14:22 +0530

    create test notebooks

---
+++ data_ingestion/loader.py
@@ -0,0 +1,5 @@
+# we are going to take raw data/documents and parse it through the code here
+#Step 1: detect file type
+#Step 2: call the right extraction function
+#Step 3: return standard structure with text + metadata
+
